b92659b8e9ed43f5bf4e3e010d8cd4cdff05b510
rakshith299 <133012343+rakshith299@users.noreply.github.com>
2023-06-21 23:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const str = "Hello world!";
+
+const output = document.getElementById("output");
+
+const random = (resolve, reject) => {
+	setTimeout(() => {
+        resolve()
+    }, 1000)
+}
+const received = new Promise(random);
+
+received.then(() => { 
+    output.innerText = str;
+    console.log("received");
+})
+
+
+received.catch(() => {
+    output.innerText = "Error";
+    console.log("rejected");
+})
